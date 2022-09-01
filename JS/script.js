9242229551d1a5e3840d122fd5b251f0bf9dd975
ElvisNarciso <elvisnarciso222@gmail.com>
2022-09-01 09:25:06 -0300

    index.html

---
+++ JS/script.js
@@ -0,0 +1,40 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+document.querySelector('#search-icon').onclick = () =>{
+    document.querySelector('#search-form').classList.toggle('active')
+}
+
+document.querySelector('#close').onclick = () =>{
+    document.querySelector('#search-form').classList.remove('active')
+}
+
+// transição de pagina
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 150,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    loop:true  
+    },
+    });
+    
+//   terminar transição
